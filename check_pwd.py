603195e6566ef704f4dfbd61a35e906152b2b32c
WenkunLiao <liaow@oregonstate.edu>
2022-02-11 23:35:19 -0800

    TDD update: Previous test passed

--- check_pwd.py
+++ check_pwd.py
@@ -6,6 +6,8 @@ def check_pwd(input):
         include_digit = False
         upper = lower.upper()
         include_upper = False
+        sym = '~`!@#$%^&*()_+-='
+        include_sym = False
         for i in input:
             if i in lower:
                 include_lower = True
@@ -13,6 +15,9 @@ def check_pwd(input):
                 include_digit = True
             if i in upper:
                 include_upper = True
-        return (include_lower and include_digit and include_upper)
+            if i in sym:
+                include_sym = True
+
+        return (include_lower and include_digit and include_upper and include_sym)
     else:
         return False
