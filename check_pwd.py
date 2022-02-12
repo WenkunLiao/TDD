f9cfa83f2063ad04e1b4e71824934e80209990f1
WenkunLiao <liaow@oregonstate.edu>
2022-02-11 23:25:37 -0800

    TDD update: Previous test passed

--- check_pwd.py
+++ check_pwd.py
@@ -2,9 +2,13 @@ def check_pwd(input):
     if 8 <= len(input) <= 20:
         lower = 'abcdefghijklmnopqrstuvwxyz'
         include_lower = False
+        digit = '1234567890'
+        include_digit = False
         for i in input:
             if i in lower:
                 include_lower = True
-        return include_lower
+            if i in digit:
+                include_digit = True
+        return (include_lower and include_digit)
     else:
         return False
