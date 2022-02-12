e0a56107594b76d0681de9128075c83a2a82efe9
WenkunLiao <liaow@oregonstate.edu>
2022-02-11 23:20:35 -0800

    TDD update: Previous test passed

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,10 @@
 def check_pwd(input):
     if 8 <= len(input) <= 20:
-        return True
+        lower = 'abcdefghijklmnopqrstuvwxyz'
+        include_lower = False
+        for i in input:
+            if i in lower:
+                include_lower = True
+        return include_lower
     else:
         return False
